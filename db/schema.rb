1eea0ffd013229be4086af01efe8d12b9250ec6c
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-07-10 20:27:59 +0300

    Creates association between user and post models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_165259) do
+ActiveRecord::Schema.define(version: 2020_07_10_171739) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
