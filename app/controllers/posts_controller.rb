85fd92d422b61dbdb850f3a879ea0362d9b68d53
Teshager <teshager8922@gmail.com>
2020-07-13 10:06:38 -0600

    Create a new view with correspoing create controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,25 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def new
-    @post = Post.new
+    @post = current_user.posts.new
   end
 
-  def create; end
+  def create
+    @post = current_user.posts.create(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
 
   def index
     @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
