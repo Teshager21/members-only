47af59ce036d526bc0afa1d15e734c2443f60d01
Teshager <teshager8922@gmail.com>
2020-07-13 01:53:07 -0600

    add user field to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, only: :create
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
