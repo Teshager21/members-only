b95dafec163b8987bbf5d5bf24f63fbcf4058c4f
Teshager <teshager8922@gmail.com>
2020-07-14 09:24:47 -0600

    Use view helper for conditional display of registration links in the nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def registration_and_session_links
+    if signed_in?
+      link_to 'Logout', destroy_user_session_path, method: :delete,
+                                                   data: { confimation: 'are you sure?' }, class: 'nav-item'
+    else
+      link_to 'Signup', new_user_registration_path, class: 'nav-item nav-link'
+
+    end
+  end
+
+  def signed_in_link
+    link_to 'Login', new_user_session_path, class: 'nav-item nav-link' unless signed_in?
+  end
 end
